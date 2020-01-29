c4352e2f9502add7009d4dc7e29c17a21f57bdbb
lizbeth58 <ortiz.beth58@gmail.com>
2020-01-29 00:38:33 -0800

    fetch function

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import React, { Component } from 'react';
 import Cardlist from './Cardlist';
 import SearchBox from './SearchBox';
-import { robots } from './robots';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: ''
     };
+  }
+
+  componentDidMount() {
+    try {
+      fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users => this.setState({ robots: users }));
+    } catch (error) {
+      alert(error);
+    }
   }
 
   onSearchChange = event => {
