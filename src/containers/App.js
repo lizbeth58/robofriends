3d546e6d70714786e159a947ca20969c893c6edf
Elizabeth Ortiz <ortiz.beth58@gmail.com>
2020-02-02 18:47:34 -0800

    saved

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import Cardlist from '../Cardlist';
-import SearchBox from '../SearchBox';
-import Scroll from '../Scroll';
+import Cardlist from '../components/Cardlist';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
+import ErrorBoundry from '../components/ErrorBoundry';
 import './App.css';
 
 class App extends Component {
@@ -38,7 +39,9 @@ class App extends Component {
         <h1 className="f1">RoboFriends</h1>
         <SearchBox searchChange={this.onSearchChange} />
         <Scroll>
+          <ErrorBoundry>
           <Cardlist robots={filteredRobots} />
+          </ErrorBoundry>
         </Scroll>
       </div>
     );
