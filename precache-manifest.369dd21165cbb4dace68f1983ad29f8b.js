6563807c6575edc0d4a0f2abf35c150de7ca6457
Elizabeth Ortiz <ortiz.beth58@gmail.com>
2020-02-02 19:15:53 -0800

    Updates

---
+++ precache-manifest.369dd21165cbb4dace68f1983ad29f8b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "550ead22c5fe8609e619026591844a2a",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "ef33967d85963ca12d00",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "f00a084132d044085147",
+    "url": "/robofriends/static/css/main.484a3289.chunk.css"
+  },
+  {
+    "revision": "ef33967d85963ca12d00",
+    "url": "/robofriends/static/js/2.b0983e84.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/robofriends/static/js/2.b0983e84.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f00a084132d044085147",
+    "url": "/robofriends/static/js/main.3c09a035.chunk.js"
+  },
+  {
+    "revision": "d6ceab57e5523008bfa1",
+    "url": "/robofriends/static/js/runtime-main.03e2f00f.js"
+  }
+]);
